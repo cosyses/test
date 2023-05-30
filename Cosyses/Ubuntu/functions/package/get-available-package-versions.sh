02b79025494e267f061646d762a693af90fe26a8
Andreas Knollmann <a.knollmann@seak.dev>
2023-05-30 23:44:11 +0200

    update test

--- Cosyses/Ubuntu/functions/package/get-available-package-versions.sh
+++ Cosyses/Ubuntu/functions/package/get-available-package-versions.sh
@@ -2,19 +2,20 @@
 
 function quoteRegex()
 {
-  sed 's/[]\.\/|\$(){}?+*^[]/\\\\&/g' <<< "\$*" | sed 's/ /\\\\\s*/g'
+  sed 's/[]\.\/|$(){}?+*^[]/\\&/g' <<< "$*" | sed 's/ /\\\s*/g'
 }
 
 packageName="${1}"
 baseVersion="${2}"
 
-if [[ -n "${baseVersion}" ]]; then
+if [ -n "${baseVersion}" ]; then
   baseVersion=$(quoteRegex "${2}")
 fi
+
 while read -r line; do
   exploded=($(echo "${line}" | tr "|" "\n"))
-  if [[ -n "${baseVersion}" ]]; then
-    if [ "$(echo "${exploded[1]}" | grep -P "^${baseVersion}" | wc -l)" -gt 0 ]; then
+  if [ -n "${baseVersion}" ]; then
+    if [ "$(echo "${exploded[1]}" | grep -P "^${baseVersion}" | wc -l )" -gt 0 ]; then
       echo "${exploded[1]}"
     fi
   else
--- Cosyses/Ubuntu/functions/package/get-installed-package-version.sh
+++ Cosyses/Ubuntu/functions/package/get-installed-package-version.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 
 packageName="${1}"
+
 /usr/bin/dpkg-query -W "${packageName}" 2>/dev/null | grep -P "^${packageName}" | cat | awk '{print $2}'
